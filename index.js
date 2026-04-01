00a61bb131991b8c16d1b38edc67b258cc2e5eaf
kelvin1 <kelvindelvinus654@gmail.com>
2026-04-01 05:27:30 -0700

    Kurekebisha puppeteer kwa ajili ya Render

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablepath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
+        args: ['--no-sandbox',
+             '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+            ],
     }
 });
 
